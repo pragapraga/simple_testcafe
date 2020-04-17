b5394f8850ea638c14712ab413b7bd6a19686266
Pragadeeswaran <pragadees24@gmail.com>
2020-04-17 12:15:39 +0530

    Adding assetions - validating "Thank you, Pragadeeswaran!" on screen

---
+++ specs/spec.js
@@ -0,0 +1,15 @@
+import { ClientFunction, Selector } from 'testcafe';
+
+fixture `Getting Started`
+    .page `http://devexpress.github.io/testcafe/example`;
+
+test('Submit your Name', async t => {
+    await t
+        .typeText('#developer-name', 'Pragadeeswaran')
+        .click('#submit-button');
+    const getLocation = ClientFunction(() => document.location.href);
+
+    await t
+        .expect(getLocation()).contains('https://devexpress.github.io/testcafe/example/thank-you.html')
+        .expect(await Selector('#article-header').textContent).eql('Thank you, Pragadeeswaran!');
+});
